526112638338b720563e98f2105fb9ecee143b24
besupernow <besupernowmchs@outlook.com>
2023-10-11 00:05:46 -0400

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./app/pages", "./app/scripts"],
+  content: ["./app/pages/loading.html"],
   theme: {
-    extend: {},
   },
-  plugins: [],
-}
-
+  plugins: [require("daisyui")],
+};
