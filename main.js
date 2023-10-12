72c9e6dadd14d835ffc42d714f7a30e0e3ccc2a8
besupernow <besupernowmchs@outlook.com>
2023-10-11 22:45:06 -0400

    true

--- main.js
+++ main.js
@@ -27,21 +27,3 @@ app.whenReady().then(() => {
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') app.quit()
 })
-const { Octokit } = require("@octokit/core");
-const octokit = new Octokit({
-    auth: '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'
-  })
-async function checkForUpdate(){
-    await octokit.request('GET /repos/Galactic-Vanguard/GameLauncher/releases/latest', {
-        owner: 'Galactic-Vanguard',
-        repo: 'GameLauncher',
-        headers: {
-          'X-GitHub-Api-Version': '2022-11-28'
-        }
-      }).then(result =>{
-        console.log(result)
-      })
-
-      
-}
-checkForUpdate()
